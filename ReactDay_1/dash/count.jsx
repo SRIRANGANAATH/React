45b5e27f3be906e5634becf12324aa15b0411dd4
SRIRANGANAATH <sriranganaath1207@gmail.com>
2025-11-08 11:58:12 +0530

    Add Counter component with increment and decrement

---
+++ ReactDay_1/dash/count.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useState } from 'react'
+function Counter() {
+  const [count, setCount] = useState(0); 
+
+  const increment = () => {
+    setCount(count + 1); 
+  };
+
+  const decrement = () => {
+    setCount(count - 1); 
+  };
+
+  return (
+ <div>
+      <h2>Count: {count}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>   
+ </div>
+);
+}
+export default Counter
